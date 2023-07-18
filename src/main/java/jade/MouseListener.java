943d2a85f667d46e266206730236097385c3c92f
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-07-18 16:09:20 +0100

    added position, button and scroll callbacks to MouseListener

--- src/main/java/jade/MouseListener.java
+++ src/main/java/jade/MouseListener.java
@@ -1,10 +1,15 @@
 package jade;
 
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
 public class MouseListener {
+  // GLFW Mouse callbacks
+
   private static MouseListener instance;
   private double scrollX, scrollY;
   private double xPos, yPos, lastY, lastX;
-  private boolean mouseButtonPressed[] = new boolean[3];
+  private boolean[] mouseButtonPressed = new boolean[3];
   private boolean isDragging;
 
   private MouseListener() {
@@ -18,5 +23,31 @@ public class MouseListener {
 
   public static MouseListener get() {
     return MouseListener.instance == null ? new MouseListener() : MouseListener.instance;
+  }
+
+  public static void mousePosCallback(long window, double xPos, double yPos) {
+    // sets previous xy coords to last positions, and updates current xy to previous xy
+    get().lastX = get().xPos;
+    get().lastY = get().yPos;
+    get().xPos = xPos;
+    get().yPos = yPos;
+  }
+
+  public static void mouseButtonCallback(long window, int button, int action, int mods) {
+    // mods are modifiers like holding shift or control during mouse press
+
+    if (button < get().mouseButtonPressed.length) {
+      if (action == GLFW_PRESS) {
+        get().mouseButtonPressed[button] = true;
+      } else if (action == GLFW_RELEASE) {
+        get().mouseButtonPressed[button] = false;
+        get().isDragging = false;
+      }
+    }
+  }
+
+  public static void mouseScrollCallback(long window, double xOffset, double yOffset) {
+    get().scrollX = xOffset;
+    get().scrollY = yOffset;
   }
 }
