a94864b87bdcfe0aa70869da0095421bd498d2c1
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-07-24 13:11:32 +0100

    removed unused statements

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -12,31 +12,6 @@ import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class LevelEditorScene extends Scene {
 
-  private String vertexShaderSrc = "#version 330 core\n" +
-          "layout (location=0) in vec3 aPos;\n" +
-          "layout (location=1) in vec4 aColor;\n" +
-          "\n" +
-          "out vec4 fColor;\n" +
-          "\n" +
-          "void main() {\n" +
-          "    fColor = aColor;\n" +
-          "    // passes the position to fragment\n" +
-          "    gl_Position = vec4(aPos, 1.0);\n" +
-          "}";
-
-  private String fragmentShaderSrc = "#version 330 core\n" +
-          "\n" +
-          "in vec4 fColor;\n" +
-          "\n" +
-          "out vec4 color;\n" +
-          "\n" +
-          "void main() {\n" +
-          "    // colors the position with fColor passed in earlier and outs it to GPU\n" +
-          "    color = fColor;\n" +
-          "}";
-
-  private int vertexID, fragmentID, shaderProgram;
-
   private float[] vertexArray = {
           // position                 // color
           // x      y     z             r     g     b     a
--- src/main/java/renderer/Shader.java
+++ src/main/java/renderer/Shader.java
@@ -3,7 +3,6 @@ package renderer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 
 import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL20.*;
