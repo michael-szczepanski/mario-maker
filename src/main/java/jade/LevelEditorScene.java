adf89757428fda94944182461a6a21ff70093aa1
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-07-31 13:56:42 +0100

    fixed Render Batch accepting more than 8 textures per batch

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -18,7 +18,7 @@ public class LevelEditorScene extends Scene {
         obj1.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/testImage.png")));
         this.addGameObjectToScene(obj1);
 
-        GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)));
+        GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(128, 128)));
         obj2.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/testImage2.png")));
         this.addGameObjectToScene(obj2);
 
