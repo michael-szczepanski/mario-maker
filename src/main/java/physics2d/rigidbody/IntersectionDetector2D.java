8473cd129978d1ccedaf5ec9aaba0af1e8005208
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-08-10 13:16:34 +0100

    raycast against a Circle implemented

--- src/main/java/physics2d/rigidbody/IntersectionDetector2D.java
+++ src/main/java/physics2d/rigidbody/IntersectionDetector2D.java
@@ -1,9 +1,7 @@
 package physics2d.rigidbody;
 
 import org.joml.Vector2f;
-import physics2d.primitives.AABB;
-import physics2d.primitives.Box2D;
-import physics2d.primitives.Circle;
+import physics2d.primitives.*;
 import renderer.Line2D;
 import util.JMath;
 
@@ -127,8 +125,39 @@ public class IntersectionDetector2D {
     }
 
     // =================================
-    // Ray vs Primitives
+    // Raycasts
     // =================================
 
+    // TODO: Unit tests
+    public static boolean raycast(Circle circle, Ray2D ray, RaycastResult result) {
+        RaycastResult.reset(result);
+        Vector2f originToCircle = new Vector2f(circle.getCenter()).sub(ray.getOrigin());
+        float radiusSq = circle.getRadius() * circle.getRadius();
+        float originToCircleLengthSq = originToCircle.lengthSquared();
 
+        // Project the vector from the ray origin onto the direction of the ray
+        float a = originToCircle.dot(ray.getDirection());
+        float bSq = originToCircleLengthSq - (a * a);
+        if (radiusSq - bSq < 0.0f) {
+            return false;
+        }
+
+        float f = (float)Math.sqrt(radiusSq - bSq);
+        float t = 0;
+        if (originToCircleLengthSq < radiusSq) {
+            // Ray starts inside the circle
+            t = a + f;
+        } else {
+            t = a - f;
+        }
+
+        if (result != null) {
+            Vector2f point = new Vector2f(ray.getOrigin()).add(ray.getDirection().mul(t));
+            Vector2f normal = new Vector2f(point).sub(circle.getCenter());
+            normal.normalize();
+            result.init(point, normal, t, true);
+        }
+
+        return true;
+    }
 }
