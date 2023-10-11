39b7c94a2e77c7a2b4db097c4b6f25cac50178cb
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-10-11 13:08:25 +0100

    Added a Collisions class and implemented a findCollisionFeatures(Circle, Circle) method

--- src/main/java/physics2d/rigidbody/CollisionManifold.java
+++ src/main/java/physics2d/rigidbody/CollisionManifold.java
@@ -2,19 +2,35 @@ package physics2d.rigidbody;
 
 import org.joml.Vector2f;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollisionManifold {
 
+    private boolean isColliding;
     private Vector2f normal;
-    private List<Vector2f> contactPoint;
+    private List<Vector2f> contactPoints;
     private float depth;
 
+    public CollisionManifold() {
+        this.normal = new Vector2f();
+        this.depth = 0.0f;
+        this.isColliding = false;
+    }
 
-    public CollisionManifold(Vector2f normal, List<Vector2f> contactPoint, float depth) {
+    public CollisionManifold(Vector2f normal, float depth) {
         this.normal = normal;
-        this.contactPoint = contactPoint;
+        this.contactPoints = new ArrayList<>();
         this.depth = depth;
+        this.isColliding = true;
+    }
+
+    //==================
+    // Helper functions
+    //==================
+
+    public void addContactPoint(Vector2f contact) {
+        this.contactPoints.add(contact);
     }
 
     //=========
